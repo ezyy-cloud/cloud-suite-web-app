2b13404fe173b019356fc8b42c0f7b9e9673f29b
kloudCoder <schneider@ezyy.cloud>
2022-03-30 12:31:18 +0200

    Added another portfolio site

--- src/views/PortfolioView.vue
+++ src/views/PortfolioView.vue
@@ -1,5 +1,5 @@
 <template>
-    <v-card height="85vh" elevation="3">
+    <v-card height="85vh" elevation="3" style="margin: 2vh;">
       <iframe src="https://pamushanaafrica.com/" height="85%" width="100%" scrolling="no" frameBorder="0"></iframe>
       <v-card-actions>
         <v-card-title>
@@ -9,6 +9,19 @@
         <v-btn size="small" color="surface-variant" href="https://www.facebook.com/PamAfriGroup/" variant="text" icon="mdi-facebook">f</v-btn>
         <v-btn size="small" color="surface-variant" href="https://twitter.com/pamafrigroup" variant="text" icon="mdi-twitter">i</v-btn>
         <v-btn size="small" color="surface-variant" href="https://www.instagram.com/_pamushana_/?hl=en" variant="text" icon="mdi-instagram">t</v-btn>
+      </v-card-actions>
+    </v-card>
+    <v-spacer />
+    <v-card height="85vh" elevation="3" style="margin: 2vh;">
+      <iframe src="https://mountviewprep.com/" height="85%" width="100%" scrolling="no" frameBorder="0"></iframe>
+      <v-card-actions>
+        <v-card-title>
+          <a href="https://mountviewprep.com" style="color: black;">Mountview Preparatory School</a>
+        </v-card-title>
+        <v-spacer />
+        <v-btn size="small" color="surface-variant" href="https://www.facebook.com/mountviewprep/" variant="text" icon="mdi-facebook">f</v-btn>
+        <v-btn size="small" color="surface-variant" href="https://twitter.com/mountviewprep" variant="text" icon="mdi-twitter">i</v-btn>
+        <v-btn size="small" color="surface-variant" href="https://www.instagram.com/mountviewprep/" variant="text" icon="mdi-instagram">t</v-btn>
       </v-card-actions>
     </v-card>
 </template>
