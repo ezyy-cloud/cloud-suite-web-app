89b415bafad9db9046a8480c35978e9502afcc37
kloudCoder <dev@ezyy.cloud>
2023-02-26 12:51:37 +0200

    Updated footer

--- src/layouts/components/Footer.vue
+++ src/layouts/components/Footer.vue
@@ -2,12 +2,13 @@
   <div class="w-100 h-100 d-flex align-center justify-space-between">
     <!-- 👉 Footer: left content -->
     <span class="d-flex align-center">
-      Made By <a
+      Another <a
         href="https://ezyy.cloud"
         target="_blank"
         rel="noopener noreferrer"
-        class="text-primary ms-1"
-      >Ezyy Cloud</a>
+        class="text-primary ms-1 me-1"
+      >Ezyy</a>
+      Cloud
     </span>
     <!-- 👉 Footer: right content -->
     <span class="d-md-flex gap-x-4 text-primary d-none">
